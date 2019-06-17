6a8655574619f5cfc969c0a906a61c2d087afa15
Alex <sokolovav2016@gmail.com>
2019-06-17 21:48:02 +0300

    Выполняет задание учебного проекта по 4 лекции

--- js/setup.js
+++ js/setup.js
@@ -37,6 +37,29 @@ var WIZARD_EYES_COLORS = [
 ];
 var NUMBER_OF_WIZARD = 4;
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
+var setupElement = document.querySelector('.setup');
+var setupOpenElement = document.querySelector('.setup-open');
+var setupCloseElement = setupElement.querySelector('.setup-close');
+var setupUserNameElement = setupElement.querySelector('.setup-user-name');
+
+var WIZARD_FIREBALL_COLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
+var setupMainWizardElement = setupElement.querySelector('.setup-wizard');
+var setupFireballElement = setupElement.querySelector('.setup-fireball-wrap');
+
+var inputHiddenCoatElement = setupElement.querySelector('input[name="coat-color"]');
+var inputHiddenEyesElement = setupElement.querySelector('input[name="eyes-color"]');
+var inputHiddenFireballElement = setupElement.querySelector('input[name="fireball-color"]');
+
 var userDialog = document.querySelector('.setup'); // Основной блок
 var similarListElement = userDialog.querySelector('.setup-similar-list'); // Список персонажей
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
@@ -78,7 +101,74 @@ function renderWizard(wizard) {
 for (var i = 0; i < wizards.length; i++) {
   fragment.appendChild(renderWizard(wizards[i]));
 }
-similarListElement.appendChild(fragment); // Сам fragment не имеет обертки?
+similarListElement.appendChild(fragment);
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+// Пытался сделать, что бы окно не закрывалось когда фокус находится на форме ввода имени с помощью focus и blur, но смог только через activeElement
+function onPopupEscPress(evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    if (document.activeElement !== setupUserNameElement) {
+      closePopup();
+    }
+  }
+}
+
+function openPopup() {
+  setupElement.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+}
+
+function closePopup() {
+  setupElement.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+}
+
+setupOpenElement.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpenElement.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
+});
+
+setupCloseElement.addEventListener('click', function () {
+  closePopup();
+});
+
+setupCloseElement.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+setupUserNameElement.addEventListener('invalid', function () {
+  if (setupUserNameElement.validity.tooShort) {
+    setupUserNameElement.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (setupUserNameElement.validity.tooLong) {
+    setupUserNameElement.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (setupUserNameElement.validity.valueMissing) {
+    setupUserNameElement.setCustomValidity('Обязательное поле');
+  } else {
+    setupUserNameElement.setCustomValidity(''); // Вот это зачем надо, не совсем ясно?
+  }
+});
+
+// Можно было бы объединить в одну функцию два слушателя ниже, повесив один слушатель на весь блок формы, но стоит ли?
+setupMainWizardElement.addEventListener('click', function (evt) {
+  if (evt.target.classList.contains('wizard-coat')) {
+    evt.target.style.fill = getRandomElement(WIZARD_COAT_COLORS);
+    inputHiddenCoatElement.value = getRandomElement(WIZARD_COAT_COLORS);
+  } else if (evt.target.classList.contains('wizard-eyes')) {
+    evt.target.style.fill = getRandomElement(WIZARD_EYES_COLORS);
+    inputHiddenEyesElement.value = getRandomElement(WIZARD_EYES_COLORS);
+  }
+});
+
+setupFireballElement.addEventListener('click', function () {
+  setupFireballElement.style.backgroundColor = getRandomElement(WIZARD_FIREBALL_COLORS);
+  inputHiddenFireballElement.value = getRandomElement(WIZARD_FIREBALL_COLORS);
+});
 
